2dbda8eb0800745df3c6f4debd54dfc2c43ac9a2
Simon <simon.bystricky@gmail.com>
2023-07-12 09:33:38 +0200

    Added basic todo list functionalities

--- src/Components/Todo/Todo.tsx
+++ src/Components/Todo/Todo.tsx
@@ -1,16 +1,47 @@
-import './Todo.css'
-import TodoTask from "./TodoTask";
+import styles from './Todo.module.css'
+import TodoTask, {TodoTaskType} from "./TodoTask";
+import {useEffect, useState} from "react";
 
 export default function Todo() {
+    const [tasks, setTasks] = useState<TodoTaskType[]>([{value: 'Task 1', id: 1}, {
+        value: 'Task 2',
+        id: 2
+    }, {value: 'Task 3', id: 3},]);
+    const [showCompleted, setShowCompleted] = useState(false);
+
+    const onTaskComplete = (id: number) => {
+        setTasks(tasks.map(a => a.id === id ? {...a, completed: true} : a))
+    }
+
+    const onTaskUncomplete = (id: number) => {
+        setTasks(tasks.map(a => a.id === id ? {...a, completed: false} : a))
+    }
+
+    const addTask = (value: string) => setTasks([...tasks, {value, id: tasks.length + 1}])
+
+    const toggleShowCompletedTasks = () => setShowCompleted(!showCompleted)
+
+    const deleteTask = (id: number) => setTasks(tasks.filter(a => a.id !== id))
 
 
-    return <div className={'container'}>
+    return <div className={styles.container}>
         <h2>Todo</h2>
 
-        <div className="task-container">
-            <TodoTask />
-            <TodoTask />
-            <TodoTask />
+        <TodoTask onValueChange={addTask} addTaskFunc={addTask}/>
+
+        <div className={styles.taskContainer}>
+            {/*Tasks*/}
+            {tasks.filter(a => !a.completed).map((task, index) => <TodoTask key={task.id} task={task}
+                onComplete={onTaskComplete} onDelete={deleteTask}/>)}
+            {/*Completed tasks toggle*/}
+            <div>
+                <h5>Completed</h5>
+                <button className={styles.showCompletedTasksButton} onClick={toggleShowCompletedTasks}>Show</button>
+            </div>
+            {/*Completed tasks*/}
+            {showCompleted && tasks.filter(a => a.completed).reverse().map((task, index) =>
+                <TodoTask key={task.id} task={task} onUncomplete={onTaskUncomplete} onDelete={deleteTask}/>)}
         </div>
+
     </div>
 }
--- src/Components/Todo/TodoTask.tsx
+++ src/Components/Todo/TodoTask.tsx
@@ -1,10 +1,39 @@
 import styles from './TodoTask.module.css'
+import {KeyboardEvent, useState} from "react";
 
-export default function TodoTask() {
+export type TodoTaskType = {
+    id: number, value: string, completed?: boolean, due?: Date,
+}
 
+type Props = {
+    task?: TodoTaskType,
+    onComplete?: (id: number) => void,
+    onUncomplete?: (id: number) => void,
+    onValueChange?: (value: string) => void,
+    onDelete?: (id: number) => void,
+    addTaskFunc?: (value: string) => void, // function to add a task
+}
 
+/**
+ * Todo task component
+ * @param props.addTaskFunc function to add a task - when provided, the component will be used to add tasks
+ */
+export default function TodoTask(props: Props) {
+    const [value, setValue] = useState(props.task?.value ?? '')
+
+    const onInputKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
+        if (!props.addTaskFunc || e.key !== 'Enter' || !value || !value.trim()) return
+
+        props.addTaskFunc(value)
+        setValue('')
+    }
 
     return <div className={styles.container}>
-        TASK
+        {!props.addTaskFunc && <button className={styles.doneButton} onClick={() => props.task?.completed ?
+            props.onUncomplete?.(props.task.id) : props.onComplete?.(props.task?.id ?? -1)}>
+            {props.task?.completed ? 'Back' : 'Done'}</button>}
+        <input onKeyDown={onInputKeyDown} type="text" placeholder={props.addTaskFunc ? 'Add task' : ''} value={value}
+               onChange={a => setValue(a.target.value)}/>
+        {!props.addTaskFunc && <button className={styles.deleteButton} onClick={() => props.onDelete!(props.task!.id)}>Delete</button>}
     </div>
 }
