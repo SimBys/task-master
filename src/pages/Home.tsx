807747f2fb04ecaf2b5234624d4b9a4239d54ad7
Simon <simon.bystricky@gmail.com>
2023-07-17 22:00:06 +0200

    chore: Update Home component UI and structure

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,30 +1,45 @@
 import React from 'react';
-import { Button } from '@mui/material';
+import {Button, Grid, Paper, Typography} from '@mui/material';
 import { Link } from 'react-router-dom';
 import styles from './Home.module.css';
 
-const Home: React.FC = () => {
+export default function Home() {
 
 	return (
 		<div className={styles.container}>
-			<div className={styles.content}>
-				<h1>Welcome to My Landing Page</h1>
-				<p>Explore the amazing features and sign up now!</p>
-				<div className={styles.buttonContainer}>
-					<Link to="/login" className={styles.link}>
-						<Button variant="contained" color="primary" className={styles.button}>
-							Log In
-						</Button>
-					</Link>
-					<Link to="/sign-up" className={styles.link}>
-						<Button variant="contained" color="secondary" className={styles.button}>
-							Sign Up
-						</Button>
-					</Link>
-				</div>
-			</div>
+			<Paper sx={{ zIndex: 1, p: 4, borderRadius: 5 }}>
+				<Grid container alignItems="center" justifyContent="center" spacing={2}>
+					<Grid item>
+						<Typography variant="h3">Welcome to</Typography>
+					</Grid>
+					<Grid item>
+						<Typography variant="h3" fontWeight={700}>
+							Task master
+						</Typography>
+					</Grid>
+				</Grid>
+				<Grid container alignItems="center" justifyContent="center" mt={2}>
+					<Grid item>
+						<Typography variant="h5">The ultimate todo list</Typography>
+					</Grid>
+				</Grid>
+				<Grid container alignItems="center" justifyContent="center" mt={2}>
+					<Grid item>
+						<Link to="/login" className={styles.link}>
+							<Button variant="contained" color="primary" className={styles.button}>
+								Log In
+							</Button>
+						</Link>
+					</Grid>
+					<Grid item ml={3}>
+						<Link to="/sign-up" className={styles.link}>
+							<Button variant="contained" color="secondary" className={styles.button}>
+								Sign Up
+							</Button>
+						</Link>
+					</Grid>
+				</Grid>
+			</Paper>
 		</div>
 	);
 };
-
-export default Home;
