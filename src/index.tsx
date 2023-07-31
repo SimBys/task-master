30d2643c2bff36b7979f7d440972096253820a9e
Simon <simon.bystricky@gmail.com>
2023-07-31 18:04:38 +0200

    set up for GitHub Pages

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import {BrowserRouter} from "react-router-dom";
+// import {BrowserRouter} from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
@@ -12,9 +13,11 @@ import '@fontsource/roboto/700.css';
 ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 ).render(
-  <React.StrictMode>
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
-  </React.StrictMode>
+    <React.StrictMode>
+        {/*<BrowserRouter>*/}
+        <Router>
+            <App />
+        </Router>
+        {/*</BrowserRouter>*/}
+    </React.StrictMode>
 );
